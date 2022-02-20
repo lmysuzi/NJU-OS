5d1d7472cc606da9986aa013c75bbeafccac84c2
tracer-nju <tracer@nju.edu.cn>
2022-02-20 09:50:49 +0800

    Linux ubuntu 5.13.0-30-generic #33~20.04.1-Ubuntu SMP Mon Feb 7 14:25:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:50:49 up 42 min,  1 user,  load average: 0.22, 0.11, 0.06

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+DIR *dir;
 
 int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+    //printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]);
+  opendir("~");
+  assert(dir!=NULL);
   return 0;
 }
