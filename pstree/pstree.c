e4dd2ff954511f577194eaa8a7151c7b5fbfd94f
tracer-nju <tracer@nju.edu.cn>
2022-02-20 10:24:21 +0800

    Linux ubuntu 5.13.0-30-generic #33~20.04.1-Ubuntu SMP Mon Feb 7 14:25:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:24:21 up  1:16,  1 user,  load average: 0.50, 0.32, 0.19

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -5,8 +5,8 @@
 #include <dirent.h>
 #include <stdbool.h>
 
-const char originPath[6]="/proc";
-const char targetFileName[5]="stat";
+const char originPath[6]="/proc/";
+const char targetFileName[5]="/stat";
 DIR *dir=NULL;
 struct dirent *dirent=NULL;
 FILE *fp=NULL;
