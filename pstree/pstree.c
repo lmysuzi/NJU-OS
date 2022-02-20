be30a9933b2c82dfae87aa083d58ebc570514e22
tracer-nju <tracer@nju.edu.cn>
2022-02-20 17:06:44 +0800

    Linux ubuntu 5.13.0-30-generic #33~20.04.1-Ubuntu SMP Mon Feb 7 14:25:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:06:44 up  1:00,  1 user,  load average: 0.35, 0.34, 0.26

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -15,13 +15,13 @@
 #define showPids "--show-pids"
 #define numericSort "--numeric-sort"
 #define version "--version"
-#define Version "pstree (PSmisc) UNKNOWN\
-Copyright (C) 1993-2019 Werner Almesberger and Craig Small\
-\
-PSmisc comes with ABSOLUTELY NO WARRANTY.\
-This is free software, and you are welcome to redistribute it under\
-the terms of the GNU General Public License.\
-For more information about these matters, see the files named COPYING."
+#define Version "pstree (PSmisc) UNKNOWN\n\
+Copyright (C) 1993-2019 Werner Almesberger and Craig Small\n\
+\n\
+PSmisc comes with ABSOLUTELY NO WARRANTY.\n\
+This is free software, and you are welcome to redistribute it under\n\
+the terms of the GNU General Public License.\n\
+For more information about these matters, see the files named COPYING.\n"
 #define print_version printf(Version);
 
 typedef struct proc{
