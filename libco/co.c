2e93194a20bf90d79432a354773769f0c146c1ff
tracer-nju <tracer@nju.edu.cn>
2022-03-08 23:14:05 +0800

    Linux ubuntu 5.13.0-30-generic #33~20.04.1-Ubuntu SMP Mon Feb 7 14:25:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:14:05 up  2:07,  1 user,  load average: 0.11, 0.27, 0.13

--- libco/co.c
+++ libco/co.c
@@ -1,106 +1,145 @@
-#include "co.h"
-#include <stdlib.h>
-#include <setjmp.h>
-#include <stdint.h>
 #include <stdio.h>
+#include <setjmp.h>
+#include <string.h>
+#include <stdlib.h>
+#include "co.h"
+#include <assert.h>
 
-#define STACK_SIZE 8192
+#define KB * 1024LL
+#define MB KB * 1024LL
+#define GB MB * 1024LL
+#define MAX_CO 10
+#define START_OF_STACK(stack) ((stack)+sizeof(stack))
 
-enum co_status {
-  CO_NEW = 1, // 新创建，还未执行过
-  CO_RUNNING, // 已经执行过
-  CO_WAITING, // 在 co_wait 上等待
-  CO_DEAD,    // 已经结束，但还未释放资源
-};
+#ifdef DEBUG
+#define Log(format, ...) \
+        printf("\33[1;34m[%s,%d,%s] " format "\33[0m\n", \
+                        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+#define Assert(cond, ...) \
+      do { \
+          if (!(cond)) { \
+                Log(__VA_ARGS__); \
+                assert(cond); \
+              } \
+        } while (0)
+#define panic(format, ...) \
+      Assert(0, format, ## __VA_ARGS__)
+#define TODO() panic("please implement me")
+static int times;
+#define debug() do {\
+    Log("DEBUG #%d", ++times);\
+    void* sp;\
+    asm volatile("mov " SP ", %0": "=g"(sp));\
+    Log("SP = %p", sp);\
+    for (int i=0;i<3;++i){\
+        Log("stackptr %d: %p", i, crs[i].stackptr);\
+    }\
+} while (0);
+#else
+#define Log(format, ...)
+#define Assert()
+#define panic()
+#define TODO()
+#define debug()
+#endif
+
+#if defined(__i386__)
+#define SP "%%esp"
+#define BP "%%ebp"
+#elif defined(__x86_64__)
+#define SP "%%rsp"
+#define BP "%%rbp"
+#endif
+
+#define changeframe(old, new)\
+  asm volatile("mov " SP ", %0" :\
+               "=g"(crs[old].stackptr));\
+  asm volatile("mov %0, " SP :\
+               :\
+               "g"(crs[new].stackptr))
+#define restoreframe(num)\
+  asm volatile("mov %0," SP : : "g"(crs[num].stackptr))
 
 struct co {
-  char *name;
-  void (*func)(void *); // co_start 指定的入口地址和参数
-  void *arg;
-
-  enum co_status status;  // 协程的状态
-  struct co *    waiter;  // 是否有其他协程在等待当前协程
-  jmp_buf        context; // 寄存器现场 (setjmp.h)
-  uint8_t        stack[STACK_SIZE]; // 协程的堆栈
-  void* rsp;
+        char name[64];
+        jmp_buf env;
+        char done;
+        void *stackptr;
+        char stack[32 KB] __attribute__ (( aligned(16) ));
 };
+struct co crs[MAX_CO];
+int co_num, cur;
 
-struct co *current=NULL;
-struct co cosn[3];
-int num=1;
+//Variable storing local variable
+func_t func_;
+void *arg_;
 
-static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
-  asm volatile (
-#if __x86_64__
-    "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
-      : : "b"((uintptr_t)sp),     "d"(entry), "a"(arg)
-#else
-    "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
-      : : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg)
-#endif
-  );
+void co_init()
+{
+        strcpy(crs[0].name, "main");
+        crs[0].stackptr = NULL;
+        co_num = cur = 0;
 }
 
-struct co *co_start(const char *name, void (*func)(void *), void *arg) {
- // cos[num].name=name;
-  cosn[num].func=func;
-  cosn[num].arg=arg;
-  cosn[num].status=CO_NEW;
-  cosn[num].waiter=NULL;
-  return &cosn[num++];
+struct co *co_start(const char *name, func_t func, void *arg)
+{
+        func_ = func;
+        arg_ = arg;
+
+        co_num++;
+        strcpy(crs[co_num].name, name);
+        crs[co_num].done = 0;
+        crs[co_num].stackptr = START_OF_STACK(crs[co_num].stack);
+
+        int ind = setjmp(crs[cur].env);
+        if (!ind) {
+                changeframe(cur, co_num);
+                cur = co_num;
+                func_(arg_);    // Test #2 hangs
+                crs[cur].done = 1;
+                co_yield();
+        }
+        restoreframe(cur);
+
+        return &(crs[co_num]);
 }
 
-void co_wait(struct co *co) {
+void co_yield()
+{
+        int pre = cur;
+        do {
+                cur = rand() % (co_num + 1);
+        } while (crs[cur].done);
+
+        int ind = setjmp(crs[pre].env);
+        debug();
+        if (!ind) {
+                changeframe(pre, cur);
+                longjmp(crs[cur].env, 1);
+        }
+        restoreframe(cur);
 }
 
-void co_yield() {
-  setjmp(current->context);
-  int chosen=rand()/num;
-  current=&cosn[chosen];
-  longjmp(cosn[chosen].context,chosen);
+void co_wait(struct co *thd)
+{
+        while (!thd->done) {
+                int ind = setjmp(crs[cur].env);
+                if (!ind) {
+                        co_yield();
+                }
+        }
 }
-
-void first(){
-  while(1){
-    printf("fuck\n");
-    co_yield();
-  }
-}
-
-void second(){
-  while(1){
-   printf("shit\n");
-    co_yield();
-  }
-}
-
 
 
 int main()
 {
-  setjmp(cosn[0].context);
-  while(1){
-  co_start(NULL,first,NULL);
-  co_start(NULL,second,NULL);
+  co_start("first",first,NULL);
   co_yield();
-  }
   return 0;
 }
 
 __attribute__((constructor))void initial(){
-  cosn[0].status=CO_RUNNING;
-  cosn[0].func=(void*)main;
-  cosn[0].rsp=cosn[0].stack+sizeof(cosn[0].stack);
-  
-  asm volatile(
-    
-    #if __x86_64__
-    "movq %0, %%rsp; jmp *%1"
-      : : "b"((uintptr_t)cosn[0].rsp),     "d"(cosn[0].func)
-#else
-    "movl %0, %%esp; jmp *%1"
-      : : "b"((uintptr_t)cosn[0].rsp - 8), "d"(cosn[0].func)
-#endif
-  );
-  current=&cosn[0];
+  srand((unsigned)time(NULL));
+  coInfo[0].status=CO_RUNNING;
+  strcpy(coInfo[0].name,"main");
 }
