b3ede2f227f783785e5abf35bd96834e83e66e7e
tracer-nju <tracer@nju.edu.cn>
2022-03-09 15:46:48 +0800

    Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:46:48 up 9 min,  1 user,  load average: 0.52, 0.45, 0.30

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -29,6 +29,7 @@ static void work(void *arg) {
 
 static void test_1() {
 
+    printf("fuck\n");
     struct co *thd1 = co_start("thread-1", work, "X");
     struct co *thd2 = co_start("thread-2", work, "Y");
 
