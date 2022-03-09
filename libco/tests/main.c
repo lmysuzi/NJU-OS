67e5ebb76ca88b91e75faa98ed480667e1119d88
tracer-nju <tracer@nju.edu.cn>
2022-03-09 16:29:17 +0800

    Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:29:17 up 18 min,  1 user,  load average: 0.10, 0.13, 0.09

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -15,6 +15,7 @@ static int get_count() {
 }
 
 static void work_loop(void *arg) {
+    printf("fucccck\n");
     const char *s = (const char*)arg;
     for (int i = 0; i < 100; ++i) {
         printf("%s%d  ", s, get_count());
@@ -24,7 +25,6 @@ static void work_loop(void *arg) {
 }
 
 static void work(void *arg) {
-    printf("fucccck\n");
     work_loop(arg);
 }
 
