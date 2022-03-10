7da29cfc379d446cfd4d94afdd699984b05ed96c
tracer-nju <tracer@nju.edu.cn>
2022-03-10 08:47:31 +0800

    Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:47:31 up 3 min,  1 user,  load average: 0.84, 0.56, 0.24

--- libco/co.c
+++ libco/co.c
@@ -125,7 +125,7 @@ begin:
       asm volatile(
       #if __x86_64__
       "movq %0, %%rsp"
-      ::"b"((uintptr_t)current->sp+8)
+      ::"b"((uintptr_t)current->sp-8)
       #else
       "movl %0, %%esp"
       ::"b"((uintptr_t)current->sp-8)
