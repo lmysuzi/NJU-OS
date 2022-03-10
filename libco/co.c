3673f4e7cf94a24ef032ae85efa55fbde8b8f62d
tracer-nju <tracer@nju.edu.cn>
2022-03-10 09:33:27 +0800

    Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:33:27 up 49 min,  1 user,  load average: 0.65, 0.64, 0.55

--- libco/co.c
+++ libco/co.c
@@ -108,7 +108,7 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
 asm volatile(
       #if __x86_64__
       "movq %0, %%rsp"
-      ::"b"((uintptr_t)current->sp)
+      ::"b"((uintptr_t)current->sp-8)
       #else
       "movl %0, %%esp"
       ::"b"((uintptr_t)current->sp-8)
@@ -138,6 +138,7 @@ void co_wait(struct co *co) {
     if(!setjmp(current->context))
     co_yield();
   }
+   printf("shit\n");
   coFree(co);
 }
 
