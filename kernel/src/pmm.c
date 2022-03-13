b194890049921744351c5b8201b7eb0c6817af15
tracer-nju <tracer@nju.edu.cn>
2022-03-13 13:55:26 +0800

    Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:55:26 up 40 min,  1 user,  load average: 0.54, 0.24, 0.16

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,21 +1,42 @@
 #include <common.h>
 
 #define MAGIC 7654321
-#define Size(size) (size+sizeof(node_t))
+#define actual(size) (size+sizeof(header_t))
 
 typedef struct node_t{
-  uintptr_t size;
+  size_t size;
   struct node_t *next;
 }node_t;
 
 typedef struct header_t{
-  uintptr_t size;
+  size_t size;
   int magic;
 }header_t;
 
+static const size_t maxSize=(16<<20);
 static node_t *head;
 
+/*static size_t tableSizeFor(size_t val){
+  if (val & (val - 1)){
+    val |= val>>1;
+    val |= val>>2;
+    val |= val>>4;
+    val |= val>>8;
+    val |= val>>16;
+    return val+1;
+  }
+  else return val == 0 ? 1 : val;
+}*/
+
 static void *kalloc(size_t size) {
+  if(size<=0||size>maxSize)return NULL;
+  //size_t sizePow=tableSizeFor(size);
+  node_t *node=head;
+  while(node!=NULL){
+    if(node->size>=actual(size)){
+
+    }
+  }
   return NULL;
 }
 
@@ -25,6 +46,7 @@ static void kfree(void *ptr) {
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+  printf("%d %d\n",sizeof(node_t),sizeof(header_t));
   node_t *Head=(node_t *)heap.start;
   Head->next=NULL,Head->size=pmsize-sizeof(node_t);
   head=Head;
