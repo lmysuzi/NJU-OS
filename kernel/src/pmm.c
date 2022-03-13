fa95563798ef0fe67bb67b5c66d8cf26c4c3fbdf
tracer-nju <tracer@nju.edu.cn>
2022-03-13 10:35:30 +0800

    Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:35:30 up 57 min,  1 user,  load average: 0.34, 0.32, 0.21

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -27,7 +27,7 @@ static void pmm_init() {
   node_t *Head=(node_t *)heap.start;
   Head->next=NULL,Head->size=pmsize-sizeof(node_t);
   head=&Head;
-  printf("%p\n",head);
+  printf("%p\n",*head);
 }
 
 MODULE_DEF(pmm) = {
