5557e20c4971c7caef3f421093461b190ee949ca
tracer-nju <tracer@nju.edu.cn>
2022-03-13 14:46:30 +0800

    Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:46:30 up  1:32,  1 user,  load average: 0.20, 0.24, 0.18

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -16,7 +16,7 @@ typedef struct header_t{
 static const size_t maxSize=(16<<20);
 static node_t *head;
 
-/*static size_t tableSizeFor(size_t val){
+static size_t tableSizeFor(size_t val){
   if (val & (val - 1)){
     val |= val>>1;
     val |= val>>2;
@@ -26,16 +26,35 @@ static node_t *head;
     return val+1;
   }
   else return val == 0 ? 1 : val;
-}*/
+}
 
 static void *kalloc(size_t size) {
   if(size<=0||size>maxSize)return NULL;
-  //size_t sizePow=tableSizeFor(size);
+  size_t sizePow=tableSizeFor(size);
+  size_t mask=~((size_t)sizePow-1);
   node_t *node=head;
   while(node!=NULL){
     if(node->size>=actual(size)){
-
+      void *iniAddr=(void*)node+sizeof(node_t);
+      void *endAddr=iniAddr+node->size;
+      void *addr=(void*)(mask&(size_t)iniAddr);
+      for(;addr<iniAddr+node->size;addr+=sizePow){
+        if(addr>=iniAddr){
+          if(addr+size+sizeof(node_t)<endAddr){
+            node_t *newAddr=(node_t*)(addr+size);
+            newAddr->size=endAddr-(void*)newAddr-sizeof(node_t);
+            newAddr->next=node->next,newAddr->prev=node->prev;
+            if(node==head)head=newAddr;
+            if(node->prev)node->prev->next=newAddr;
+            if(node->next)node->next->prev=newAddr;
+          }
+          header_t *header=(header_t*)(addr-sizeof(header_t));
+          header->size=size,header->magic=MAGIC;
+          return addr;
+        }
+      }
     }
+    node=node->next;
   }
   return NULL;
 }
@@ -48,8 +67,10 @@ static void pmm_init() {
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
   node_t *Head=(node_t *)heap.start;
   printf("%d %d\n",sizeof(header_t),sizeof(node_t));
-  Head->next=NULL,Head->size=pmsize-sizeof(node_t);
+  Head->prev=NULL,Head->next=NULL,Head->size=pmsize-sizeof(node_t);
   head=Head;
+  int* a=kalloc(sizeof(int));
+  *a=1;
 }
 
 MODULE_DEF(pmm) = {
