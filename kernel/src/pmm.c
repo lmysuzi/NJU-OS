8333fdcc295cf9610cc05afea93ea97cd75ecdf9
tracer-nju <tracer@nju.edu.cn>
2022-03-17 14:55:23 +0800

    Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:55:23 up  1:06,  1 user,  load average: 0.27, 0.24, 0.11

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -93,8 +93,8 @@ static void *kalloc(size_t size) {
     if(node->size>=actual(size)){
       void *iniAddr=(void*)node+sizeof(node_t);
       void *endAddr=iniAddr+node->size;
-      printf("%p\n",endAddr);
       void *addr=(void*)(mask&(size_t)iniAddr);
+      printf("%x\n",addr);
       for(;addr<iniAddr+node->size;addr+=sizePow){
         if(addr>=iniAddr){
           if(addr+size+sizeof(node_t)<endAddr){
