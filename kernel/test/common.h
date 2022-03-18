425f901b7502cd349cbb091b888bfc1e24d41e53
tracer-nju <tracer@nju.edu.cn>
2022-03-18 14:45:38 +0800

    Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:45:38 up  1:42,  1 user,  load average: 0.46, 0.17, 0.15

--- kernel/test/common.h
+++ kernel/test/common.h
@@ -1,3 +1,5 @@
 #include <kernel.h>
 //#include <klib.h>
 //#include <klib-macros.h>
+#include "../../abstract-machine/klib/include/klib-macros.h"
+#include "../../abstract-machine/klib/include/klib.h"
