c71129bcaf4efc38b57ca45410ff3110712130c0
tracer-nju <tracer@nju.edu.cn>
2022-03-19 17:00:50 +0800

    Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:00:50 up 37 min,  1 user,  load average: 0.23, 0.22, 0.25

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -147,13 +147,13 @@ static void *kalloc(size_t size) {
             if(node==head)head=newAddr;
             if(node->prev){
               node->prev->next=newAddr;
-              if((void*)node->prev+actual(node->prev->size)==(void*)node)node->prev->size=addr-(void*)node->prev-2*sizeof(node_t);
+              // if((void*)node->prev+actual(node->prev->size)==(void*)node)node->prev->size=addr-(void*)node->prev-2*sizeof(node_t);
             }
             if(node->next)node->next->prev=newAddr;
           }
           else{//该内存块分完
             if(head==node)head=node->next;
-            if(node->prev)if((void*)node->prev+actual(node->prev->size)==(void*)node)node->prev->size=addr-(void*)node->prev-2*sizeof(node_t);
+            //if(node->prev)if((void*)node->prev+actual(node->prev->size)==(void*)node)node->prev->size=addr-(void*)node->prev-2*sizeof(node_t);
             if(node->next)node->next->prev=node->prev;
           }
           header_t *header=(header_t*)(addr-sizeof(node_t));
