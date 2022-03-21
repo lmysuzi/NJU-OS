535dde51a149e9c9c728d60aeca844ed83c7bde4
tracer-nju <tracer@nju.edu.cn>
2022-03-21 22:46:52 +0800

    Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:46:52 up 45 min,  1 user,  load average: 0.31, 0.43, 0.34

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -126,8 +126,8 @@ static void *kalloc(size_t size) {
             if(head==node)head=node->next;
             if(node->prev){
               node->prev->next=node->next;
+              if((void*)node->prev+actual(node->prev->size)==(void*)node)node->prev->size=addr-(void*)node->prev-2*sizeof(node_t);
             }
-            if(node->prev)if((void*)node->prev+actual(node->prev->size)==(void*)node)node->prev->size=addr-(void*)node->prev-2*sizeof(node_t);
             if(node->next)node->next->prev=node->prev;
           }
           header_t *header=(header_t*)(addr-sizeof(node_t));
