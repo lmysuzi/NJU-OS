7babb0e6a249b66e78897ba6322cc1f3ad9c4dbb
tracer-nju <tracer@nju.edu.cn>
2022-03-21 22:49:19 +0800

    Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:49:19 up 47 min,  1 user,  load average: 0.81, 0.66, 0.44

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -76,12 +76,12 @@ static void insert(node_t *new){
 
 static void merge(){
   node_t *temp=head;
-  while(temp){
+  /*while(temp){
     printf("%p %x %x\n",temp,temp->size,(void*)temp+temp->size+sizeof(node_t));
     temp=temp->next;
   }
   mark;
-  temp=head;
+  temp=head;*/
   while(temp){
     if(temp->next&&(void*)temp+actual(temp->size)==(void*)temp->next){
       temp->size+=actual(temp->next->size);
@@ -89,12 +89,12 @@ static void merge(){
     }
     else temp=temp->next;
   }
-  temp=head;
+  /*temp=head;
   while(temp){
     printf("%p %x %x\n",temp,temp->size,(void*)temp+temp->size+sizeof(node_t));
     temp=temp->next;
   }
-  printf("shit\n");
+  printf("shit\n");*/
 }
 
 static void *kalloc(size_t size) {
