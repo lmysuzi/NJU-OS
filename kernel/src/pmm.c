abe59c983ad30bf1b337fa584ffc5012f91f8213
tracer-nju <tracer@nju.edu.cn>
2022-03-21 22:31:51 +0800

    Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:31:51 up 30 min,  1 user,  load average: 0.15, 0.18, 0.20

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -42,7 +42,7 @@ typedef struct header_t{
 static const size_t maxSize=(16<<20);
 static node_t *head;
 static lock_t pmmLock;
-//static int count=0;//记录free的次数，定期merge
+static int count=0;//记录free的次数，定期merge
 
 static size_t tableSizeFor(size_t val){
   if (val & (val - 1)){
@@ -57,10 +57,12 @@ static size_t tableSizeFor(size_t val){
 }
 
 static void insert(node_t *new){
+  if(new<head){
     new->next=head,new->prev=NULL;
     head->prev=new;head=new;
     return;
-  /*node_t *temp=head;
+  }
+  node_t *temp=head;
   while(temp!=NULL){
     if(temp->next==NULL||temp->next>new){
       new->next=temp->next,new->prev=temp;
@@ -69,15 +71,15 @@ static void insert(node_t *new){
       return;
     }
     temp=temp->next;
-  }*/
+  }
 }
 
-/*static void merge(){
+static void merge(){
   node_t *temp=head;
-  while(temp){
+  /*while(temp){
     //printf("%p %x %x\n",temp,temp->size,(void*)temp+temp->size+sizeof(node_t));
     temp=temp->next;
-  }
+  }*/
   while(temp){
     if(temp->next&&(void*)temp+actual(temp->size)==(void*)temp->next){
       temp->size+=actual(temp->next->size);
@@ -85,7 +87,7 @@ static void insert(node_t *new){
     }
     else temp=temp->next;
   }
-}*/
+}
 
 static void *kalloc(size_t size) {
   if(size<=0||size>maxSize)return NULL;
@@ -141,7 +143,7 @@ static void kfree(void *ptr) {
   node_t *new=(node_t*)header;
   new->size=size;
   insert(new);
-  //if((++count)==COUNT)count=0,merge();
+  if((++count)==COUNT)count=0,merge();
   unlock(&pmmLock);
 }
 
@@ -152,6 +154,7 @@ static void pmm_init() {
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
   head=(node_t*)heap.start;
   head->prev=NULL,head->next=NULL,head->size=pmsize-sizeof(node_t);
+  printf("%d %d\n",sizeof(header_t),sizeof(node_t));
   init(&pmmLock);
 }
 #else
