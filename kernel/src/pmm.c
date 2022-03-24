a2ffacb37077d0ccb32832b1d5b423ed8ff3329f
tracer-nju <tracer@nju.edu.cn>
2022-03-24 08:35:04 +0800

    Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:35:04 up  8:24,  1 user,  load average: 0.32, 0.21, 0.17

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -66,7 +66,7 @@ static void kfree(void *ptr) {
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
-  printf("%d\n",orderOfPage(heap.start));
+  printf("%d\n",orderOfPage(heap.end));
   printf("%d\n",pmsize/PAGESIZE);
   printf("%d\n",cpu_count());
 }
