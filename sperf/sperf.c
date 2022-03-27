f783fe227a6d456fe6c0f87a629d8a75035ef9c9
tracer-nju <tracer@nju.edu.cn>
2022-03-27 12:44:12 +0800

    Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:44:12 up  1:06,  1 user,  load average: 0.42, 0.16, 0.13

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
+char command[100];
 int main(int argc, char *argv[]) {
   /*char *exec_argv[] = { "strace","-C", "ls", NULL, };
   char *exec_envp[] = { "PATH=/bin", NULL, };
   execve("strace",          exec_argv, exec_envp);
   execve("/bin/strace",     exec_argv, exec_envp);
   execve("/usr/bin/strace", exec_argv, exec_envp);*/
-  for(int i=0;i<argc;i++)printf("%s\n",argv[i]);
+  char *a="xingyinga\n";
+  printf("%s",a);
   perror(argv[0]);
   exit(EXIT_FAILURE);
 }
