e5d423cc4e94b207ab0f75070a3f4b68c975e670
tracer-nju <tracer@nju.edu.cn>
2022-03-27 12:14:08 +0800

    Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:14:08 up 35 min,  1 user,  load average: 0.45, 0.34, 0.27

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  char *exec_argv[] = { "strace","-tt", "ls", NULL, };
+  char *exec_argv[] = { "strace","-C", "ls", NULL, };
   char *exec_envp[] = { "PATH=/bin", NULL, };
   execve("strace",          exec_argv, exec_envp);
   execve("/bin/strace",     exec_argv, exec_envp);
