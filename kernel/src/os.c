b4b5fff56a6e172e30c123629c9216ab0a525888
tracer-nju <tracer@nju.edu.cn>
2022-03-27 16:35:30 +0800

    Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:35:30 up  2:51,  1 user,  load average: 0.43, 0.40, 0.26

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -8,15 +8,6 @@ static void os_run() {
   for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
     putch(*s == '*' ? '0' + cpu_current() : *s);
   }
-  /*void*a[100];
-  for(int i=99;i;i--){
-    a[i]=pmm->alloc(128);
-   // printf("%x\n",a[i]);
-  }
-  for(int i=99;i;i--){
-    if(a[i])
-    pmm->free(a[i]);
-  }*/
   while (1) ;
 }
 
