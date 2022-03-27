4a246366b24bb20d6e0cc7519266ccf2d43eff00
tracer-nju <tracer@nju.edu.cn>
2022-03-27 16:18:07 +0800

    Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:18:07 up  2:33,  1 user,  load average: 0.10, 0.16, 0.09

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -10,11 +10,30 @@ int main(int argc, char *argv[]) {
   exec_argv[0]="strace",exec_argv[1]="-tt";
   for(int i=1;i<argc;i++)exec_argv[i+1]=argv[i];
   exec_argv[argc+1]=NULL;
+  int pipefd[2];
+  if(pipe(pipefd)==-1){
+    perror("pipe");
+    exit(EXIT_FAILURE);
+  }
   pid_t pid=fork();
-  if(!pid){
+  if(pid<0){
+    perror("fork");
+    exit(EXIT_FAILURE);
+  }
+  else if(pid==0){
+    close(pipefd[0]);
+    dup2(pipefd[1],STDERR_FILENO);
+    close(STDERR_FILENO);
     execve("/usr/bin/strace", exec_argv, exec_envp);
   }
-  else return 0;
+  else{
+    close(pipefd[1]);
+    char temp[100];
+    FILE *fp=fopen(pipefd[0],'r');
+    fgets(fp,99,temp);
+    printf("%s\n",temp);
+    return 0;
+  } 
   perror(argv[0]);
   exit(EXIT_FAILURE);
 }
