58833c60185cbd4daeed55af5ff830dd63088aba
tracer-nju <tracer@nju.edu.cn>
2022-03-30 23:13:33 +0800

    Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:13:33 up  2:36,  1 user,  load average: 0.43, 0.22, 0.11

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
     regcomp(&reg,timePattern,REG_EXTENDED | REG_NEWLINE);
     regmatch_t pos;
     while(fgets(buf,4096,fp)!=NULL){
-      printf("%s",buf+strlen(buf)-1);
+      printf("%d\n",strlen(buf));
     }
     return 0;
   } 
