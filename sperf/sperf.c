65e237ad83010e90d8c330d6b0bb6a5cd08df3db
tracer-nju <tracer@nju.edu.cn>
2022-03-30 21:37:48 +0800

    Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:37:48 up  1:00,  1 user,  load average: 0.08, 0.11, 0.13

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -5,10 +5,7 @@
 
 int main(int argc, char *argv[]) {
   char *exec_envp[] = { "PATH=/bin", NULL, };
-  char *exec_argv[4];
-  exec_argv[0]="strace",exec_argv[1]="-tt";
-  exec_argv[2]="ls";
-  exec_argv[3]=NULL;
+  char *exec_argv[]={"strace","-tt","ls",NULL};
   int pipefd[2];
   if(pipe(pipefd)==-1){
     perror("pipe");
@@ -22,15 +19,15 @@ int main(int argc, char *argv[]) {
   if(pid==0){
     close(pipefd[0]);
     dup2(pipefd[1],STDERR_FILENO);
-    //close(STDERR_FILENO);
     close(STDOUT_FILENO);
     execve("/bin/strace",     exec_argv, exec_envp);
   }
   else{
     close(pipefd[1]);
-    char buf=0;
-    while(read(pipefd[0],&buf,1)>0){
-      printf("%c",buf);
+    char buf[4096];
+    FILE *fp=fdopen(pipefd[0],"r");
+    while(fgets(buf,4096,fp)!=NULL){
+      printf("%s",buf);
     }
     return 0;
   } 
