1691d97cfc887fa50f8bfa7a9f867012a6d0b25e
tracer-nju <tracer@nju.edu.cn>
2022-03-30 21:25:54 +0800

    Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:25:54 up 48 min,  1 user,  load average: 0.30, 0.23, 0.19

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
   if(pid==0){
     close(pipefd[0]);
     dup2(pipefd[1],STDERR_FILENO);
-    close(STDERR_FILENO);
+    //close(STDERR_FILENO);
     close(STDOUT_FILENO);
     execve("/bin/strace",     exec_argv, exec_envp);
   }
