3c2f3d2b0ebd55ca00960ab372fa878f090a371f
tracer-nju <tracer@nju.edu.cn>
2022-03-30 21:00:24 +0800

    Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:00:24 up 22 min,  1 user,  load average: 0.21, 0.15, 0.15

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  char *exec_envp[] = { "PATH=/bin", NULL, };
+  //char *exec_envp[] = { "PATH=/bin", NULL, };
   char *exec_argv[argc+2];
   exec_argv[0]="strace",exec_argv[1]="-tt";
   for(int i=1;i<argc;i++)exec_argv[i+1]=argv[i];
