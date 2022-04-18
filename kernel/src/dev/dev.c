bc3b9773b47b415e6a661aeda1b662aef5a21f86
tracer-nju <tracer@nju.edu.cn>
2022-04-18 15:03:47 +0800

    Linux ubuntu 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:03:47 up 15 min,  1 user,  load average: 0.48, 0.35, 0.22

--- kernel/src/dev/dev.c
+++ kernel/src/dev/dev.c
@@ -40,7 +40,10 @@ static void dev_init() {
   devices[id] = dev_create(sizeof(device_type), dev_name, dev_id, dev_ops); \
   devices[id]->ops->init(devices[id]);
 
-  DEVICES(INIT);
+  //DEVICES(INIT);
+  devices[0] = dev_create(sizeof(input_t), "input", 1, &input_ops); \
+  devices[0]->ops->init(devices[0]);
+  printf("fuck\n");
 
   kmt->create(pmm->alloc(sizeof(task_t)), "input-task", dev_input_task, NULL);
   kmt->create(pmm->alloc(sizeof(task_t)), "tty-task",   dev_tty_task,   NULL);
