b9f17fe47ef9f986858b7dab3b42b1bc17650978
tracer-nju <tracer@nju.edu.cn>
2022-04-24 20:32:16 +0800

    Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:32:16 up 45 min,  1 user,  load average: 0.28, 0.20, 0.20

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -18,6 +18,7 @@ int main(int argc, char *argv[]) {
       FILE *fp=fdopen(file,"w");
       fwrite("fuck",1,4,fp);
       char buf[100];
+      fseek(fp,0,SEEK_SET);
       fread(buf,1,4,fp);
       printf("%s\n",buf);
 
