7baf263eb37336a7a5bd36ff90fa1bc5bca2725a
tracer-nju <tracer@nju.edu.cn>
2022-04-25 23:28:30 +0800

    Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:28:30 up 8 min,  1 user,  load average: 0.48, 0.32, 0.17

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -45,6 +45,7 @@ int main(int argc, char *argv[]) {
       printf("%s\n",so_path);
       FILE *fp_c=fopen(c_path,"w+");
       FILE *fp_so=fopen(so_path,"w+");
+      fwrite(line,1,4096,fp_c);
       assert(fp_so!=NULL);
       assert(fp_c!=NULL);
       
