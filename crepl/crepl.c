8646fda31135e2630d9ffe710b8e56b5b934d6c0
tracer-nju <tracer@nju.edu.cn>
2022-04-25 22:45:31 +0800

    Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:45:31 up  1:12,  1 user,  load average: 0.31, 0.27, 0.26

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -5,6 +5,18 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+char *myArgv[]={
+  "gcc",
+  #if __X86_64__
+  "-m64",
+  #else
+  "-m32",
+  #endif
+  "-w","-xc","-fPIC","-shared","-o",NULL,NULL,NULL
+};
+
+static int file_num=0;
+
 int main(int argc, char *argv[]) {
   mkdir("/tmp/crepl_temp",S_IRWXU|S_IRWXG|S_IRWXO);
   static char line[4096];
@@ -16,12 +28,13 @@ int main(int argc, char *argv[]) {
       break;
     }
     if(strncmp(line,"int",3)==0){
-      char name[]="fuck.cXXXXXX";
-      int file=mkstemp(name);
-      printf("%d\n",file);
-      unlink(name);
-      close(file);
-      fopen("/tmp/crepl_temp/fuck.txt","w");
+      char path[100]="/tmp/crepl_temp/";
+      char filename[100];
+      memset(filename,0,100*sizeof(char));
+      sprintf(filename,"file%d",file_num);
+      strcat(path,filename);
+      printf("%s\n",path);
+
 
 
     }
