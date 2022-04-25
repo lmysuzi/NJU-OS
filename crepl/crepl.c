f7c489aa36616702aaeb1f8ca04441072ee54ece
tracer-nju <tracer@nju.edu.cn>
2022-04-25 23:51:12 +0800

    Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:51:12 up 31 min,  1 user,  load average: 0.33, 0.18, 0.16

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -56,7 +56,7 @@ int main(int argc, char *argv[],char *env[]) {
       else{
         wait(NULL);
         void *handle=dlopen(so_path,RTLD_NOW);
-        int (*func)(void*)=dlsym(handle,"a");
+        int (*func)(void)=dlsym(handle,"a");
         printf("%d\n",func());
       }
     }
