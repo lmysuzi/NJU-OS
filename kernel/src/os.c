bf2f8b975f1b653edf31c77fa8b2f2fd1479fc51
tracer-nju <tracer@nju.edu.cn>
2022-04-29 15:21:28 +0800

    Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:21:28 up 41 min,  1 user,  load average: 0.25, 0.21, 0.18

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -22,7 +22,7 @@ static void os_init() {
   kmt->sem_init(&empty, "empty", 2);  // 缓冲区大小为 5
   kmt->sem_init(&fill,  "fill",  0);
   for (int i = 0; i < 1; i++) // 4 个生产者
-    kmt->create(pmm->alloc(sizeof(task_t)), "producer", producer, (void*)1);
+    kmt->create(pmm->alloc(sizeof(task_t)), "producer", producer, (void*)(size_t)i);
   for (int i = 0; i < 1; i++) // 5 个消费者
     kmt->create(pmm->alloc(sizeof(task_t)), "consumer", consumer, NULL);
 }
