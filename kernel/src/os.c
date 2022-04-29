8fb3d2723fa80f885356fc29033e87db56c1f3cf
tracer-nju <tracer@nju.edu.cn>
2022-04-29 15:25:12 +0800

    Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:25:12 up 45 min,  1 user,  load average: 0.34, 0.26, 0.20

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -10,8 +10,8 @@ sem_t empty, fill;
 #define P kmt->sem_wait
 #define V kmt->sem_signal
 
-void producer(void *arg) { while (1) { P(&empty); printf("\n%d",(size_t)arg);putch('('); V(&fill);  } }
-void consumer(void *arg) { while (1) { P(&fill);  printf("\n%d",(size_t)arg);putch(')'); V(&empty); } }
+void producer(void *arg) { while (1) { P(&empty); printf(" %d",(size_t)arg);putch('('); V(&fill);  } }
+void consumer(void *arg) { while (1) { P(&fill);  printf(" %d",(size_t)arg);putch(')'); V(&empty); } }
 
 
 static void os_init() {
