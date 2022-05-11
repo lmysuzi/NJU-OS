b981037be5a255a5b9733b29a42294794e0e3271
tracer-nju <tracer@nju.edu.cn>
2022-05-11 14:26:51 +0800

    Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:26:51 up 14 min,  1 user,  load average: 0.37, 0.52, 0.33

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -109,17 +109,18 @@ kmt_schedule(Event ev,Context *context){
 
   //if(task->status==TASK_READY)current=task;
   //else current=idle;
-  //task_t *task_begin=task;
+  task_t *task_begin=task;
   /*for(int i=0;i<2;i++){
     if(task->status==TASK_READY)break;
     if(task->next)task=task->next;
     else task=task_head;
   }*/
-  /*do{
+  do{
     if(task->status==TASK_READY)break;
+    if(task->status==TASK_LOAD)task->status=TASK_READY;
     if(task->next)task=task->next;
     else task=task_head;
-  }while(task!=task_begin);*/
+  }while(task!=task_begin);
 
   if(current->status==TASK_RUNNING){
     current->status=TASK_LOAD;
@@ -127,7 +128,6 @@ kmt_schedule(Event ev,Context *context){
 
   current=task;
   if(current->status!=TASK_READY){
-    if(current->status==TASK_LOAD)current->status=TASK_READY;
     current=idle;
   }
   current->status=TASK_RUNNING;
