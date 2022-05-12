dd763aa885b6a367822e9f8e9a4acfef705bf30b
tracer-nju <tracer@nju.edu.cn>
2022-05-12 11:56:46 +0800

    Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:56:46 up 6 min,  1 user,  load average: 0.36, 0.38, 0.21

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -83,11 +83,11 @@ kmt_context_save(Event ev,Context *context){
 static Context *
 kmt_schedule(Event ev,Context *context){
   //panic_on(current==NULL,"current is null");
- /* task_t *temp=task_head;
+  task_t *temp=task_head;
   while(temp){
     printf("%d ",temp->status);
     temp=temp->next;
-  }printf("\n");*/
+  }printf("\n");
 
   /*if(current!=idle){
     if(current->status!=TASK_SLEEP)last=current;
