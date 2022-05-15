c042b23eb4dfd6467c719d0ebd4f31cad9bd01fb
tracer-nju <tracer@nju.edu.cn>
2022-05-15 11:08:41 +0800

    Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:08:41 up  2:59,  1 user,  load average: 0.12, 0.05, 0.05

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -131,7 +131,7 @@ kmt_schedule(Event ev,Context *context){
     if(task->status==TASK_LOAD)task->status=TASK_FUCK;
     else if(task->status==TASK_FUCK)task->status=TASK_SHIT;
     else if(task->status==TASK_SHIT)task->status=TASK_READY;
-    if(task->next)task=task->next;
+    if(task->next!=NULL)task=task->next;
     else task=task_head;
   }while(task!=task_begin);
 
