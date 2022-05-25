a1dfa6ba2ccb706dedfe6850f448e415c3c7a93e
tracer-nju <tracer@nju.edu.cn>
2022-05-25 08:58:21 +0800

    Linux ubuntu 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:58:21 up 22 min,  1 user,  load average: 0.69, 0.47, 0.33

--- kernel/src/uproc.c
+++ kernel/src/uproc.c
@@ -3,5 +3,56 @@
 
 #include "initcode.inc"
 
+static void init(){
+
+}
+
+static int kputc(task_t *task, char ch){
+  return 0;
+}
+
+static int fork(task_t *task){
+  return 0;
+
+}
+static int wait(task_t *task, int *status){
+  return 0;
+
+}
+
+static int exit(task_t *task, int status){
+  return 0;
+}
+static int kill(task_t *task, int pid){
+  return 0;
+
+}
+static void *mmap(task_t *task, void *addr, int length, int prot, int flags){
+  return NULL;
+}
+static int getpid(task_t *task){
+  return 0;
+
+}
+
+static int sleep(task_t *task, int seconds){
+  return 0;
+
+}
+
+static int64_t uptime(task_t *task){
+  return 0;
+
+}
 MODULE_DEF(uproc) = {
+  .init=init,
+  .kputc=kputc,
+  .fork=fork,
+  .wait=wait,
+  .exit=exit,
+  .kill=kill,
+  .mmap=mmap,
+  .getpid=getpid,
+  .sleep=sleep,
+  .uptime=uptime,
 };
