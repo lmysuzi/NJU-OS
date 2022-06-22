38a663f83f83e3f82f4305188a2e29503f825453
tracer-nju <tracer@nju.edu.cn>
2022-06-22 22:06:08 +0800

    Linux ubuntu 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:06:08 up  4:57,  1 user,  load average: 0.32, 0.29, 0.17

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -165,7 +165,7 @@ int is_dir(DIR *dir){
 
 int inline isbmp(bmp_t *bmp,u32 size){
   if(bmp->id[0]!='B'||bmp->id[1]!='M')return 0;
-  if(bmp->size!=size)return 0;
+  //if(bmp->size!=size)return 0;
   return 1;
 }
 
