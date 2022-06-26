33443f380a45cd66677d11b1f0a2beb56e203711
tracer-nju <tracer@nju.edu.cn>
2022-06-26 18:37:51 +0800

    Linux ubuntu 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:37:51 up 6 min,  1 user,  load average: 0.34, 0.32, 0.16

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -95,6 +95,7 @@ kmt_task_wake(Event ev,Context *context){
     sleep_tasks_t *temp=node->next;
     if(io_read(AM_TIMER_UPTIME).us>=node->time){
       sleep_delete(node);
+      mark;
     }
     node=temp;
   }
