c0067954376f4e3663f4a2e260d78cd033a6ebe2
tracer-nju <tracer@nju.edu.cn>
2022-06-27 11:12:07 +0800

    Linux ubuntu 5.13.0-51-generic #58~20.04.1-Ubuntu SMP Tue Jun 14 11:29:12 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:12:07 up  2:02,  1 user,  load average: 0.64, 0.80, 0.68

--- kernel/src/uproc.c
+++ kernel/src/uproc.c
@@ -192,6 +192,7 @@ static int
 sleep(task_t *task, int seconds){
   int us=io_read(AM_TIMER_UPTIME).us+seconds*1000000;
   sleep_insert(task_now(),us);
+  printf("fuck\n");
   yield();
   return 0;
 }
